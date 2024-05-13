5913f57d776210eb1bfc8137429c5d37f843e887
WBLT4U <156853865+WBLT4U@users.noreply.github.com>
2024-05-13 09:10:17 +0100

    Update contactus.js

--- src/components/contactus.js
+++ src/components/contactus.js
@@ -1,20 +1,26 @@
-//Create a homepage component...
-const ContactUs = () => {
+//Handle button click
+function ContactUs() {
+  //Render
+  
   return (
-    <section className="home-page">
-           <h1>
-        <p>
-          CONTACT US <br></br>
-          Address: No. 12 Third Avenue Federal Housing Rumjin Sambo Sokoto-Nigeria
-          <br></br>
-          Tel: +2348066713592, +2348157225633
-          <br></br>
-          Email: support@wbilt.com.ng
-          <br></br>
-        </p>
-      </h1>
+    <section className="form-container">
+    <form 
+    onSubmit={(event) => {
+      event.preveantDefault();
+      const { name, email, tel, username, password } = event.target.elements;
+      console.log(name.value, email.value, tel.value, username.value, password.value);
+      alert(
+        `Hello ${name.value}! Your email is ${email.value} Your tel is ${tel.value}! Your username is ${username.value}! and your password is ${password.value}`
+        );
+    }}>
+      <input type="text" placeholder="Enter your Full Name" name="name"/>
+      <input type="text" placeholder="Enter your Email" name="email" />
+      <input type="text" placeholder="Enter your Phone" name="tel" />
+      <input type="text" placeholder="Enter your Username" name="username" />
+      <input type="text" placeholder="Enter your Password" name="password" />
+      <button type="submit">Submit</button>
+    </form>
     </section>
   );
-};
-
+}
 export default ContactUs;
