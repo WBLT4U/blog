7c401978baf294fabc1d52abb0e2838f309300ce
WBLT4U <156853865+WBLT4U@users.noreply.github.com>
2024-05-13 08:55:11 +0100

    Update aboutus.js

--- src/components/aboutus.js
+++ src/components/aboutus.js
@@ -1,38 +1,24 @@
-//Create a homepage component...
-const AboutUs = () => {
+//Handle button click
+function AboutUs() {
+  //Render
+  
   return (
-    <section className="home-page">
-      <h1>
-        <p>
-        ABOUT US <br></br>
-          WBLT Tech Limited is a company that provide exceptional Software
-          Development, Mobile App Development, CCTV Camera installation, Solar
-          Energy Installation, Networking and technology services, tailored to
-          meet your goals. WBLT TECH is a cutting-edge digital solutions
-          provider. We bring our clients' digital ideas to life by offering
-          premium Website & Mobile application development services, Digital
-          Marketing, Custom & Turn-key software creation, and IT implementation,
-          and support.
-        </p>
-      </h1>
-      <h1>
-        <p>
-          OUR MISSION <br></br>
-          Our mission is to be a leading integrated Information and
-          Communication Technology (ICT) solution company in Nigeria, making use
-          of technically-skilled, experienced and dedicated workforce with a
-          sound capital base channeled towards maintaining an excellent service
-          delivery culture, centered on robust customer satisfaction that helps
-          us generate good returns to the benefit of all stakeholders.
-          <br></br>
-          OUR VISION <br></br>
-          Our vision is to become the most revered one-stop shop integrated
-          Information Technology services organization in Nigeria with great
-          potential to dominate the West-African sub-region. We know
-        </p>
-      </h1>
+    <section className="form-container">
+    <form 
+    onSubmit={(event) => {
+      event.preveantDefault();
+      const { name, email, tel } = event.target.elements;
+      console.log(username.value, password.value);
+      alert(
+        `Hello ${name.value}! Your email is ${email.value} and your tel is ${tel.value}`
+        );
+    }}>
+      <input type="text" placeholder="Enter your Username" name="username"/>
+      <input type="password" placeholder="Enter your Password" name="password" />
+      
+      <button type="submit">Submit</button>
+    </form>
     </section>
   );
-};
-
+}
 export default AboutUs;
